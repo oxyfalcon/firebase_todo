db77bd027c9c83e63da1e97ea0900dbab350e1f5
oxyfalcon <akashsinghcool19990@gmail.com>
2023-08-25 22:32:48 +0530

    test_commit_bonami_mac

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:app/screens/done_list_screen/marked_home_page.dart';
 import 'package:app/screens/todo_list_screen/my_home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
-}
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,10 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           home: const MyHomePage(),
           title: "Todo",
-          initialRoute: '/',
-          routes: {'/marked': (context) => const MarkedHomePage()},
           theme: ThemeData(
             colorScheme:
                 ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
